fcd13c23ff671e090af0a2fe957af12f3de7849e
shria-devarakonda <shriadevarakonda@gmail.com>
2022-03-20 20:15:21 +0530

    updated for db

---
+++ addserialkillers.py
@@ -0,0 +1,44 @@
+import sqlite3 as sl
+
+
+def addtodb(id, name, victim_count, famous_for, moniker, age_at_arrest):
+    con = sl.connect('serial_killer.db')
+    with con:
+
+        try:
+            con.execute("""
+                CREATE TABLE KILLERS (
+                    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    victimcount INTEGER,
+                    famousfor TEXT,
+                    moniker TEXT,
+                    ageatarrest INTEGER
+                );
+            """)
+        except:
+            pass
+
+        sql = 'INSERT INTO KILLERS (id, name, victimcount, famousfor, moniker, ageatarrest) values(?, ?, ' \
+            '?,?,?,?) '
+
+    data = [
+        (id, name, victim_count, famous_for, moniker, age_at_arrest)
+    ]
+    try:
+        with con:
+            con.executemany(sql, data)
+    except Exception as e:
+        print(e)
+
+
+def add_queries():
+    id = input("Please provide an id for the serial killer (in integers): ")
+    name = input("Please provide the name: ")
+    victim_count = input("Please provide the victim count (in integers): ")
+    famous_for = input("Please provide the details on the signature of the serial killer: ")
+    moniker = input("Please provide the moniker of the serial killer: ")
+    age_at_arrest = input("Please provide the age of the serial killer at his arrest: ")
+    addtodb(id, name,victim_count,famous_for,moniker,age_at_arrest)
+
+add_queries()
--- serialkillers.py
+++ serialkillers.py
@@ -1,25 +1,24 @@
-#this is the main serial killer object creator
+# Object for serial killer data display
+import sqlite3 as sl
+
 
 class SerialKiller:
-    def __init__(self, name, victims_count, mental_disability, legacy, moniker, agearrest):
+    def __init__(self, idnum, name, victims_count, legacy, moniker, agearrest):
         self.name = name
+        self.idnum = idnum
         self.victims_count = victims_count
-        self.mental_disability = mental_disability
         self.legacy = legacy
         self.moniker = moniker
         self.agearrest = agearrest
 
+    def id_num(self):
+        return "ID: {}.\n".format(self.idnum)
+
     def nameofsk(self):
-        return " His name was {}.\n".format(self.name)
+        return "His name was {}.\n".format(self.name)
       
     def vicnum(self):
         return "He killed {} people.\n".format(self.victims_count)
-      
-    def mental(self):
-        if self.mental_disability:
-            return "He had a mental disability.\n"
-        else:
-            return "He had no mental disability.\n"
           
     def bestknown(self):
         return "He was best known for {}.\n".format(self.legacy)
@@ -30,26 +29,28 @@ class SerialKiller:
     def arrestedage(self):
         return "He was arrested when he was {}.".format(self.agearrest)
 
+sk = []
+sk_name = []
+con = sl.connect('serial_killer.db')
+with con:
+     data = con.execute("SELECT * FROM KILLERS")
+     for row in data:
+        sk.append(row)
 
-jeff =["Jeffrey Dahmer", 17, False, "trying to rip out a victims heart from his chest", "The Milwaukee Monster", 32]
-ed =["Ed Kemper", 10, True, "decapitating his mother's head and engaging in irrumatio with it", "The Co-ed Killer", 25]
-ted = ["Ted Bundy", 30, False, "being deceitfully charming and evading arrest many times", 'The Lady Killer', 29]
-john = ["John Wayne Gacy", 33, False, "dressing like a clown and killing his victims", 'Pogo the Clown', 36]
-richard = ["Richard Ramirez", 13, True, "being a satan worshipper and having a large female fan following after arrest", 'The Night Stalker', 25]
-dahmer = SerialKiller(*jeff)
-kemper = SerialKiller(*ed)
-bundy = SerialKiller(*ted)
-gacy = SerialKiller(*john)
-ramirez = SerialKiller(*richard)
+with con:
+    data = con.execute("SELECT name FROM KILLERS")
+    for row in data:
+        sk_name.append(row)
+import re
+a = re.sub("]*\\[*\\(*\\)*'*", '', str(sk_name))
+c = re.sub("(,)", '|', a)
+print(c+"|")
+name = input("Who would you like to learn about? Enter any of the names between the '||'s above. Partial name is "
+             "fine too.\nFor instance, you can type jeff or dahm for Jeffrey Dahmer.\nIf there is not much "
+             "above, please add some data with addserialkiller.py file!\n").lower()
 
-def infosk(name):
-    print(name.nameofsk(), name.vicnum(), name.mental(), name.bestknown(), name.nickname(), name.arrestedage())
 
-sk = ["dahmer", "kemper", "bundy", "gacy", "ramirez"]
-print([i for i in sk])
-query = input("Who would you like to learn about? (Enter any of the last names above) \n").lower()
-
-try:
-    infosk(eval(query))
-except:
-    print("Sorry, no information on {}.".format(query))
+for row in sk:
+    if str(name) in str(row).lower():
+        name = SerialKiller(*list(row))
+        print(name.id_num(), name.nameofsk(), name.vicnum(), name.bestknown(), name.nickname(), name.arrestedage())
